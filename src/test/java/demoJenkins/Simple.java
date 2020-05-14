36b7a7242df132f786d6300f4adf902dfafcbdef
remijullian <33925686+remijullian@users.noreply.github.com>
2020-05-14 19:08:23 +0530

    Remi's 1st commit

---
+++ src/test/java/demoJenkins/Simple.java
@@ -0,0 +1,19 @@
+package demoJenkins;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Simple {
+	
+	@Test
+	public void testcase() {
+		System.out.println("Welcome to Maven Jenkins");
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\Drivers\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+
+}
